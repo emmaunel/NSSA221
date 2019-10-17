e2ad89d0ca2fdb537af1441a9b1eb21415f85c10
Emmanuel <aadewale123@gmail.com>
2019-10-17 10:29:20 -0400

    ass 2

--- ass2/ass2.sh
+++ ass2/ass2.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-FILE="user.csv"
-tmp="/tmp/user.csv"
-NEW_FILE="$(tail +2 $FILE > $tmp)"
-IFS=","
-head="$(head -n 1 $FILE)"
-IFS=', ' read -r -a head_array <<< "$head"
-head_length=${#head_array[@]}
+#FILE="user.csv"
+#tmp="/tmp/user.csv"
+#NEW_FILE="$(tail +2 $FILE > $tmp)"
+#IFS=","
+#head="$(head -n 1 $FILE)"
+#IFS=', ' read -r -a head_array <<< "$head"
+#head_length=${#head_array[@]}
+#
+#main(){
+#    while read LINE; do
+#            IFS=', ' read -r -a array <<< "$LINE"
+#            line_length=${#array[@]}
+#
+#            for (( i = 0 ; i < $line_length - 1 ; i++)); do
+#                echo "${head_array[$i]} [$i]: ${array[$i]}"
+#
+#            done
+#            echo " "
+#
+#    done < $tmp
+#}
+#
 
-main(){
-    while read LINE; do
-            IFS=', ' read -r -a array <<< "$LINE"
-            line_length=${#array[@]}
-
-            for (( i = 0 ; i < $line_length - 1 ; i++)); do
-                echo "${head_array[$i]} [$i]: ${array[$i]}"
-                if[["${array[$i]}" = " "]];
-                    echo "hi"
-                fi
-            done
-            echo " "
-
-    done < $tmp
-}
-
-
-main
