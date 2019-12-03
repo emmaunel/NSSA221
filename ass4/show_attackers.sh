1b80647a6a65c8730b358bf42fd1b8b8806bd6e1
Emmanuel <aadewale123@gmail.com>
2019-12-03 10:55:33 -0500

    again easy

--- ass4/show_attackers.sh
+++ ass4/show_attackers.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-file=$1
-#check if geoip is install, if not install it
-
-if [ -f "$file" ]; then
-      echo "Exists"
-      cat $file | grep Failed > /tmp/filter
+# Ayobami Adewale --> Script 4(easy)
+if [ -f "$1" ]; then
+      cat $1 | grep Failed > /tmp/filter
 else
-      echo "Cannot open log file: $file"
+      echo "Cannot open log file: $1"
       exit 1
 fi
 
-# need to get all the ip seperately
+# Get all the ip seperately and saves it temporarly
 cat /tmp/filter | cut -d " " -f 11 | sort -u | grep '[0-9].[0-9].' > /tmp/ips
 
-#grep -o 'ip' test | wc -l
+#CSV Header
+printf '%s\n' Count IP Location | paste -sd ',' >> logs.csv
+
+# Parse the ips and location into a csv file
 while read line; do
-	 echo $line;
- 	 num=$(grep -o $line /tmp/filter | wc -l)
-     	 [ $num -lt 10 ] && echo "ignore for now" || geoiplookup $line | cut -d " " -f 5
+      # Gets the number of times an ip appears
+ 	num=$(grep -o $line /tmp/filter | wc -l)
+      # if less than 10 ignore, else get the location and save it the csv
+     	if [ $num -lt 10 ]; then
+            true
+      else
+            location=$(geoiplookup $line | cut -d " " -f 5,6)
+            printf '%s\n' $num $line $location | paste -sd ',' >> logs.csv
+      fi
 done < /tmp/ips
 
-echo "Don't forget to delete /tmp/filter and /tmp/ips"
+#delete temp files and displaying results
+rm /tmp/filter
+rm /tmp/ips
+cat logs.csv
